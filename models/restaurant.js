02558ba573592b305ce8987dd5ca839352228a4a
Md. Meraj Hossain <meraj@nerddevs.com>
2024-05-07 14:45:24 +0600

    first commit

---
+++ models/restaurant.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const RestaurantSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    time: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    foods: { type: Array, default: [] },
+    pickup: { type: Boolean, required: true },
+    delivery: { type: Boolean, required: true },
+    isAvailable: { type: Boolean, required: true },
+    owner: { type: String, required: true },
+    code: { type: String, required: true },
+    logoUrl: { type: String, required: true },
+    rating: { type: Number, min: 1, max: 5, default: 3 },
+    ratingCount: { type: String, default: "167" },
+    verification: { type: String, default: "Pending", enum: ["Pending", "Verified", "Rejected"] },
+    verificationMessage: { type: String, default: "Your restaurant is under review. We will get back to you soon." },
+    coords: {
+        id: { type: String },
+        latitude: { type: Number, required: true },
+        longitude: { type: Number, required: true },
+        latitudeDelta: { type: Number, default: 0.0922 },
+        longitudeDelta: { type: Number, default: 0.0421 },
+        address: { type: String, required: true },
+        title: { type: String, required: true },
+    },
+});
+
+module.exports = mongoose.model("Restaurant", RestaurantSchema);
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const CategoryRoute = require('./routes/category');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURL)
+.then(() => console.log('Foody Connected to MongoDB'))
+.catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/categories', CategoryRoute);
+
+
+app.listen(process.env.PORT || 6013, () => console.log(`Foody Backend is running on ${process.env.PORT}!`));
